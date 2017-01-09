081151021dcee309aca620c1f8fc30d0c9f5b24a
Timo van der Schuit <timo.vander.schuit@globalrelay.net>
2017-01-08 17:44:16 -0800

    Instantiating firebase db and add newly created Question model to it on submitting form.

--- app/src/main/java/com/learn/cards/MainActivity.java
+++ app/src/main/java/com/learn/cards/MainActivity.java
@@ -3,28 +3,31 @@ package com.learn.cards;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
+import com.learn.cards.models.Question;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_FIREBASE = "FIREBASE";
+    public static final String DATABASE_QUESTIONS =  "questions";
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mDatabase = database.getReference();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,9 +48,12 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = getIntent();
             String question = intent.getStringExtra(CardFormActivity.MESSAGE_QUESTION);
             if (question != null) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
-                myRef.setValue(question);
+
+                DatabaseReference ref = mDatabase.child(DATABASE_QUESTIONS).push();
+                String questionUUID = ref.getKey();
+
+                ref.setValue(new Question(question, "my answer"));
+
                 fragment.addItem(question, R.drawable.great_wall_of_china);
             }
 
---
+++ app/src/main/java/com/learn/cards/models/Question.java
@@ -0,0 +1,21 @@
+package com.learn.cards.models;
+
+
+public class Question {
+
+    private String question;
+    private String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
